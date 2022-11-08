7d7e1c14768cb5ace9305bb5190978f6d0cb92ad
jaea-kim <jaeakim98@gmail.com>
2022-11-09 01:31:23 +0900

    소스트리 테스트 원상복구

--- week2/Mission1/src/question1/Question1.java
+++ week2/Mission1/src/question1/Question1.java
@@ -12,7 +12,6 @@ public class Question1 {
         //문자 출력
         for (int i = 0; i < character.length; i++) {
             System.out.println(character[i]);
-            System.out.println("lala");
         }
     }
 }
