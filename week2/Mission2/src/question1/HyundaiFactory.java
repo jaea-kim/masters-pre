4dd9cb6211165bb53f5c6f59f65c81a5f3e04082
jaea-kim <jaeakim98@gmail.com>
2022-11-10 16:53:05 +0900

    미션#2-1 자동차 공장 문제 풀이

--- week2/Mission2/src/question1/HyundaiFactory.java
+++ week2/Mission2/src/question1/HyundaiFactory.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public class HyundaiFactory {
     private int carNumber = 1000;
-    private static HyundaiFactory factory = new HyundaiFactory();
-    private HyundaiFactory() { }//단 하나 factory 객체 생성
+    private static HyundaiFactory factory = new HyundaiFactory(); //단 하나 factory 객체 생성
+    private HyundaiFactory() { }
     static HyundaiFactory getFactory() {
         return factory;
     }
