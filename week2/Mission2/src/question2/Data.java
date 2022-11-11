04eb3bb166ea5b8afc207bf64678410abdcbfe7f
jaea-kim <jaeakim98@gmail.com>
2022-11-12 02:44:57 +0900

    기능 - 점수 수정

--- week2/Mission2/src/question2/Data.java
+++ week2/Mission2/src/question2/Data.java
@@ -6,6 +6,12 @@ import static question2.School.studentData;
 import static question2.School.subjectCode;
 
 public class Data {
+    private static Data dataCenter = new Data();
+
+    private Data() { }
+    static Data getInstance() {
+        return dataCenter;
+    }
     public void dataProcessing(String line) {
         Map<String, Integer> gradeData = new HashMap<>();
         for (int i = 0; i < 3; i++) {
@@ -16,5 +22,26 @@ public class Data {
             gradeData.put(subjectCode[i], -1);
         }
         studentData.add(new Student(line.split("/")[0], line.split("/")[1], line.split("/")[2], gradeData));
+    }{}
+
+    public void modifyData(String studentName, String line) {
+        String[][] data = new String[line.split("/").length][2];
+        for (int i = 0; i < line.split("/").length; i++) {
+            data[i][0] = line.split("/")[i].split(" ")[0];
+            data[i][1] = line.split("/")[i].split(" ")[1];
+        }
+
+        for (int i = 0; i < studentData.size(); i++) {
+                if (studentData.get(i).getStudentInfo().equals(studentName)) {
+                    for(String[] d : data) studentData.get(i).getGradeData().put(d[0], Integer.parseInt(d[1]));
+                    break;
+                }
+        }
     }
+/*
+    private void changeGradeData(String subject, String score) {
+        gradeData
+    }
+
+ */
 }
--- week2/Mission2/src/question2/School.java
+++ week2/Mission2/src/question2/School.java
@@ -2,14 +2,17 @@ package question2;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class School {
     public static ArrayList<Student> studentData = new ArrayList<Student>();
     public static String[] subjectCode = {"국어", "수학", "영어"};
+
     public static void main(String[] args) throws IOException {
         File gradeFile = new File("./src/question2/input.txt");
-        gradeFile.storageData();
+        Data dataCenter = Data.getInstance();
+        gradeFile.storageData(dataCenter);
 
         System.out.println(" === 학생 별 총점과 평균점수 ===");
         for (int i = 0; i <studentData.size(); i++) {
@@ -17,5 +20,23 @@ public class School {
             System.out.printf("총점은 %d점이고 평균은 %d입니다.\n", studentData.get(i).totalGrade(), studentData.get(i).averageGrade());
             System.out.println("-----------------------------------------------");
         }
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.printf("실행할 기능을 선택해주세요 (0: 조회 1: 수정, 2: 추가, 3: 종료) >>> ");
+            int function = scanner.nextInt();
+
+            if (function == 3) break;
+            if (function == 1) {
+                //System.out.println("수정할 이름을 입력하세요.");
+                //String name = scanner.nextLine();
+                //System.out.println("수정할 과목명과 점수를 입력하세요.ex) 국어 50/수학 40");
+                //String line = scanner.nextLine();
+                //dataCenter.modifyData(name, line);
+                dataCenter.modifyData("Kim", "수학 80/국어 50");
+            }
+        }
+
+
     }
 }
